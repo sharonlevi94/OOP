0296a45586e2a2df3d44acb966237b34ae8a5d72
Eliad Karni <70060127+EliadKarni@users.noreply.github.com>
2020-10-31 12:10:32 +0200

    Update EquilateralTriangle.cpp

--- src/EquilateralTriangle.cpp
+++ src/EquilateralTriangle.cpp
@@ -1,88 +1,41 @@
-#include "IsoscelesTriangle.h"
-
+#include "EquilateralTriangle.h"
 //----------------------------------------------------------------------------
-IsoscelesTriangle::IsoscelesTriangle(const Vertex vertices[3]) {
-	*this = 
-		IsoscelesTriangle(
-			vertices[0], vertices[1], 
-			vertices[3].m_row - vertices[0].m_row);
+EquilateralTriangle::EquilateralTriangle() {
+	this->triangle = IsoscelesTriangle();
 }
 //----------------------------------------------------------------------------
-IsoscelesTriangle::IsoscelesTriangle(const Vertex& v0, 
-	const Vertex& v1, double height) {
-	if (v0.isValid && v1.isValid && v0.m_row == v1.m_row 
-		&& v1.isToTheRightOf(v0)) {
-		this->vertices[0] = v0;
-		this->vertices[1] = v1;
-		this->vertices[2] = 
-			Vertex((v1.m_col - v0.m_col) / 2, v1.m_row + height);
-		return;
-	}
-	*this = IsoscelesTriangle();
+EquilateralTriangle::EquilateralTriangle(Vertex vertices[3]) {
+	this->triangle = IsoscelesTriangle(vertices);
 }
 //----------------------------------------------------------------------------
-IsoscelesTriangle::getVertex(int index) const {
-	return (this->vertices[index % 3]);
+EquilateralTriangle::getVertex(int index) const {
+	return(this->triangle.getVertex(index));
 }
 //----------------------------------------------------------------------------
-IsoscelesTriangle::getBaseLength() const {
-	return(this->vertices[1].m_col - this->vertices[0].m_col);
+EquilateralTriangle::getLength() {
+	return(this->triangle.getLegLength());
 }
 //----------------------------------------------------------------------------
-IsoscelesTriangle::getLegLength() const {
-	return(
-		sqrt(
-			pow(this->getHeight(), 2) + pow(this->getBaseLength() / 2, 2)));
+EquilateralTriangle::draw(Board& board)const {
+	this->triangle.draw(board);
 }
 //----------------------------------------------------------------------------
-IsoscelesTriangle::getHeight() const {
-	return(this->vertices[3].m_row - this->vertices[0].m_row);
+EquilateralTriangle::getBoundingRectangle() const {
+	return(this->triangle.getBoundingRectangle());
 }
 //----------------------------------------------------------------------------
-IsoscelesTriangle:: draw(Board& board)const {
-	board.drawLine(this->vertices[0], this->vertices[1]);
-	board.drawLine(this->vertices[0], this->vertices[2]);
-	board.drawLine(this->vertices[1], this->vertices[2]);
+EquilateralTriangle::getArea() const {
+	return(this->triangle.getArea());
 }
 //----------------------------------------------------------------------------
-IsoscelesTriangle::getBoundingRectangle() const {
-	return (
-		Rectangle(this->vertices[0]), 
-		Vertex(this->vertices[1].m_col, this->vertices[3].m_row));
+EquilateralTriangle::getPerimeter() const {
+	return(this->triangle.getPerimeter());
 }
 //----------------------------------------------------------------------------
-IsoscelesTriangle::getArea() const {
-	return ((this->getHeight() * this->getBaseLength()) / 2);
+EquilateralTriangle::getCenter() const {
+	return(this->triangle.getCenter);
 }
 //----------------------------------------------------------------------------
-IsoscelesTriangle::getPerimeter() const {
-	return((2 * this->getLegLength()) + this->getBaseLength());
-}
-//----------------------------------------------------------------------------
-IsoscelesTriangle::getCenter() const {
-	double x = 0, y = 0;
-	for (int i = 0; i < 3; ++i) {
-		x += this->vertices[i].m_col;
-		y += this->vertices[i].m_row;
-	}
-	return(Vertex(x / 3, y / 3));
-}
-//----------------------------------------------------------------------------
-IsoscelesTriangle::scale(double factor) {
-	if (Vertex(this->vertices[0].m_col - factor,
-		this->vertices[0].m_row - factor).isValid() &&
-		Vertex(this->vertices[1].m_col + factor,
-			this->vertices[1].m_row - factor).isValid() &&
-		Vertex(this->vertices[2].m_col,
-			this->vertices[2].m_row + factor).isValid()&&)
-	{
-		*this->vertices[0] = Vertex(this->vertices[0].m_col - factor,
-			this->vertices[0].m_row - factor);
-		*this->vertices[1] = Vertex(this->vertices[1].m_col + factor,
-			this->vertices[1].m_row - factor);
-		*this->vertices[2] = Vertex(this->vertices[2].m_col,
-			this->vertices[2].m_row + factor);
-		return true;
-	}
-	return false;
+EquilateralTriangle::scale(double factor) {
+	return(this->triangle.scale(factor));
 }
