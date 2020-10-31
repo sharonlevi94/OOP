d49837955fb986df96e9b24b1ec6c95318a15777
Eliad Karni <70060127+EliadKarni@users.noreply.github.com>
2020-10-31 12:11:11 +0200

    Update IsoscelesTriangle.h

--- include/IsoscelesTriangle.h
+++ include/IsoscelesTriangle.h
@@ -1,9 +1,28 @@
 #pragma once
+#include "Vertex.h"
+#include "Rectangle.h"
+
 class IsoscelesTriangle
 {
-private:
 public:
-	
+	IsoscelesTriangle(const Vertex vertices[3]);
+	IsoscelesTriangle(
+		const Vertex& v0 = Vertex(20,20), 
+		const Vertex& v1 = Vertex(30,20), 
+		double height = sqrt(75));
 
+	Vertex getVertex(int index) const;
+	double getBaseLength() const;
+	double getLegLength() const;
+	double getHeight() const;
+
+	void draw(Board& board)const;
+	Rectangle getBoundingRectangle() const;
+	double getArea() const;
+	double getPerimeter() const;
+	Vertex getCenter() const;
+	bool scale(double factor);
+
+private:
+	Vertex vertices[3];
 };
-
