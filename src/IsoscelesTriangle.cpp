64cc987a061b089860bf7692b5f1ae847ea95ca1
Eliad Karni <70060127+EliadKarni@users.noreply.github.com>
2020-11-03 21:39:12 +0200

    Update IsoscelesTriangle.cpp

--- src/IsoscelesTriangle.cpp
+++ src/IsoscelesTriangle.cpp
@@ -1,6 +1,14 @@
+/* IsoscelesTriangle
+ * ===========================================================================
+ */
+//---------------------------- include section -------------------------------
 #include "IsoscelesTriangle.h"
-
-//----------------------------------------------------------------------------
+//-------------------------- constractors section ----------------------------
+/*----------------------------------------------------------------------------
+ * The constractor is building  the object using array of 3 received Vertexes.
+ * input: array of 3 Vertexes.
+ * output: none.
+*/
 IsoscelesTriangle::IsoscelesTriangle(const Vertex vertices[3])
 {
 	if (vertices[2].m_col == (vertices[0].m_col + vertices->m_col) / 2,
@@ -12,7 +20,12 @@ IsoscelesTriangle::IsoscelesTriangle(const Vertex vertices[3])
 	else
 		*this = IsoscelesTriangle();
 }
-//----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------
+ * The constractor is building the object using the bottom left vertex,
+ * bottom right Vertex and the triangle height.
+ * input: array of 3 Vertexes.
+ * output: none.
+*/
 IsoscelesTriangle::IsoscelesTriangle(const Vertex& v0,
 	const Vertex& v1, double height) {
 	if (v0.isValid() && v1.isValid() && v0.m_row == v1.m_row
@@ -25,46 +38,84 @@ IsoscelesTriangle::IsoscelesTriangle(const Vertex& v0,
 	}
 	*this = IsoscelesTriangle();
 }
-//----------------------------------------------------------------------------
+//---------------------------- methods section -------------------------------
+/*----------------------------------------------------------------------------
+ * The method is returning the wanted triangles Vertex,
+ * usig the received index.
+ * input: Index.
+ * output: The wanted triangles Vertex.
+*/
 Vertex IsoscelesTriangle::getVertex(int index) const {
 	return (this->vertices[index % 3]);
 }
-//----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------
+ * The method is calculates and return the lenght of the triangle's base edge.
+ * input: None.
+ * output: The lenght of the triangle base edge.
+*/
 double IsoscelesTriangle::getBaseLength() const {
 	return(this->vertices[1].m_col - this->vertices[0].m_col);
 }
-//----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------
+  The method is calculates and return the lenght of the triangle's leg edge.
+ * input: none.
+ * output: The lenght of the triangle's leg edge.
+*/
 double IsoscelesTriangle::getLegLength() const {
 	return(
 		sqrt(
 			pow(this->getHeight(), 2) + pow(this->getBaseLength() / 2, 2)));
 }
-//----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------
+ * The method calculates and return the triangle height.
+ * input: none.
+ * output: The triangle height.
+*/
 double IsoscelesTriangle::getHeight() const {
 	return(this->vertices[2].m_row - this->vertices[0].m_row);
 }
-//----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------
+ * The method drawing the triangle using the received Board.
+ * input: the board wonted the triangle drawed on.
+ * output: none.
+*/
 void IsoscelesTriangle::draw(Board& board)const {
 	board.drawLine(this->vertices[0], this->vertices[1]);
 	board.drawLine(this->vertices[0], this->vertices[2]);
 	board.drawLine(this->vertices[1], this->vertices[2]);
 }
-//----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------
+ * The method calculates and return the triangle's bounding Rectangle.
+ * input: none.
+ * output: the triangle's bounding Rectangle.
+*/
 Rectangle IsoscelesTriangle::getBoundingRectangle() const 
 {
 	return ( Rectangle(this->vertices[0],
 		     Vertex(this->vertices[1].m_col,
 				 this->vertices[2].m_row)));
 }
-//----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------
+ * The method calculates and return the trangle's area.
+ * input: none.
+ * output: triangle's area.
+*/
 double IsoscelesTriangle::getArea() const {
 	return ((this->getHeight() * this->getBaseLength()) / 2);
 }
-//----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------
+ * The method calculates and return the triangle's perimeter. 
+ * input: none.
+ * output: The triangle's perimeter.
+*/
 double IsoscelesTriangle::getPerimeter() const {
 	return((2 * this->getLegLength()) + this->getBaseLength());
 }
-//----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------
+ * The method calculates and return the triangle's center Vertex.
+ * input: none.
+ * output: triangle center Vertex.
+*/
 Vertex IsoscelesTriangle::getCenter() const {
 	double x = 0, y = 0;
 	for (int i = 0; i < 3; ++i) {
@@ -73,22 +124,28 @@ Vertex IsoscelesTriangle::getCenter() const {
 	}
 	return(Vertex(x / 3, y / 3));
 }
-//----------------------------------------------------------------------------
+/*----------------------------------------------------------------------------
+ * The method is duplicate the triangle size, check if the new sized triangle
+ * is valid, if it is its resize the triangle's values and retrun true, 
+ * if the new sized trinle isn't valid the function don't change the 
+ * triangle's values, and return false.
+ * input: the wanted resize value.
+ * output: if the resize succeed.
+*/
 bool IsoscelesTriangle::scale(double factor) {
-	if (Vertex(this->vertices[0].m_col - factor,
-		this->vertices[0].m_row - factor).isValid() &&
-		Vertex(this->vertices[1].m_col + factor,
-			this->vertices[1].m_row - factor).isValid() &&
-		Vertex(this->vertices[2].m_col,
-			this->vertices[2].m_row + factor).isValid())
-	{
-		this->vertices[0] = Vertex(this->vertices[0].m_col - factor,
-			this->vertices[0].m_row - factor);
-		this->vertices[1] = Vertex(this->vertices[1].m_col + factor,
-			this->vertices[1].m_row - factor);
-		this->vertices[2] = Vertex(this->vertices[2].m_col,
-			this->vertices[2].m_row + factor);
-		return true;
+	Vertex vercheck[3];
+	for (int i = 0; i < 3; ++i) {
+		vercheck = this->vertices[i];
+		vercheck[i].m_col -= this->getCenter().m_col;
+		vercheck[i].m_row -= this->getCenter().m_row;
+		vercheck[i].m_col *= factor;
+		vercheck[i].m_row *= factor;
+		vercheck[i].m_col += this->getCenter().m_col;
+		vercheck[i].m_row += this->getCenter().m_row;
+		if (!vercheck[i].isValid())
+			return false;
 	}
-	return false;
+	for (int i = 0; i < 3; ++i)
+		this->vertices[i] = vercheck[i];
+	return true;
 }
