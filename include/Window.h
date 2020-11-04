5fe933c319070d3e739276b11834767d9a78a3c1
sharonlevi94 <73107447+sharonlevi94@users.noreply.github.com>
2020-11-04 10:52:33 +0200

    Add files via upload

--- include/Window.h
+++ include/Window.h
@@ -1,5 +1,26 @@
 #pragma once
+#include "Vertex.h"
+#include "Board.h"
+#include "Rectangle.h"
 class Window
 {
+private:
+	Rectangle Outer, Inner;
+public:
+	Window(const Rectangle& outer, const Rectangle& inner);
+	Window(const Rectangle& outer, double verticalThickness, double horizontalThickness);
+	
+	Vertex getBottomLeft() const;
+	Vertex getTopRight()const;
+
+	double getVerticalThickness()const;
+	double getHorizontalThickness() const;
+
+	void draw(Board& board)const;
+	Rectangle getBoundingRectangle() const;
+	double getArea()const;
+	double getPerimeter()const;
+	Vertex getCenter()const;
+	bool scale(double factor);
 };
 
