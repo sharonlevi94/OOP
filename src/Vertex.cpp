0a88b23de1866ffb28aa4893acee6fb70bf676c9
sharonlevi94 <73107447+sharonlevi94@users.noreply.github.com>
2020-11-04 10:58:56 +0200

    Add files via upload

---
+++ src/Vertex.cpp
@@ -0,0 +1,37 @@
+#include "Vertex.h"
+#include "macros.h"
+
+#include <iostream>
+
+bool Vertex::isEqual(const Vertex& other) const
+{
+    if (m_col == other.m_col)
+        if (m_row == other.m_row)
+            return true;
+
+    return false;
+}
+bool Vertex::isHigherThan(const Vertex& other) const
+{
+    return m_row > other.m_row;
+}
+
+bool Vertex::isToTheRightOf(const Vertex& other) const
+{
+    return m_col > other.m_col;
+}
+
+bool Vertex::isValid() const
+{
+    return m_col >= 0 && m_col <= MAX_COL && m_row >= 0 && m_row <= MAX_ROW;
+}
+
+std::istream& operator>>(std::istream& istr, Vertex& v)
+{
+    return istr >> v.m_col >> v.m_row;
+}
+
+std::ostream& operator<<(std::ostream& ostr, const Vertex& v)
+{
+    return ostr << "(" << v.m_col << ", " << v.m_row << ")";
+}
